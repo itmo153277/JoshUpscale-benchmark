3a3241eb00300ac382904f0422a1030bbca8b589
viktprog <viktprog@gmail.com>
2022-01-07 14:13:37 +0900

    Fix ms in log format

--- src/logging.cc
+++ src/logging.cc
@@ -15,11 +15,14 @@ namespace logging {
 
 std::ostream &operator<<(std::ostream &os, const LogTimestamp &ts) {
 	auto timeStruct = LogTimestamp::clock::to_time_t(ts);
+	auto flags = os.flags();
 	os << std::put_time(std::localtime(&timeStruct), "%Y-%m-%d %H:%M:%S") << '.'
+	   << std::setw(3) << std::setfill('0')
 	   << (std::chrono::duration_cast<std::chrono::milliseconds>(
 	           ts.time_since_epoch()) %
 	          1000)
 	          .count();
+	os.flags(flags);
 	return os;
 }
 
